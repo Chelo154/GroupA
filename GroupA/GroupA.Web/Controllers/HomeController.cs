64eabfbc4717fb38de55710aa143518833e1fb45
chelo154 <chelo154@gmail.com>
2019-11-08 01:03:38 -0300

    Cambios al Controlador y al About.cshtml

--- GroupA/GroupA.Web/Controllers/HomeController.cs
+++ GroupA/GroupA.Web/Controllers/HomeController.cs
@@ -10,12 +10,20 @@ namespace GroupA.Web.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.Message = "Nosotros";
+            ViewBag.Mision = "Ser una empresa encargada del desarrollo de sistemas Web para empresas ofreciendo un servicio de alta calidad,tecnología confiable y diseños de vanguardia. Maximizando el potencial de nuestros clientes a través del internet.";
+            ViewBag.Vision = "Ser la empresa de desarrolladores de Páginas Web más elegida y cotizada por su compromiso, originalidad  y capacidad de potenciar a nuestros clientes en el mercado digital.";
             ViewBag.NombreEmpresa = "Group A";
+            
             return View();
         }
 
         public ActionResult About()
         {
+            ViewBag.LoremIpsum = @"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
             ViewBag.Message = "Nosotros";
             ViewBag.Mision = "Ser una empresa encargada del desarrollo de sistemas Web para empresas ofreciendo un servicio de alta calidad,tecnología confiable y diseños de vanguardia. Maximizando el potencial de nuestros clientes a través del internet.";
             ViewBag.Vision = "Ser la empresa de desarrolladores de Páginas Web más elegida y cotizada por su compromiso, originalidad  y capacidad de potenciar a nuestros clientes en el mercado digital.";
